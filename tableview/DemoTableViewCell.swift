6b00e3d23411c95b652fef4881694e9cd6bdfd78
Vijay Kumar <munukotivjiaykumar@gmail.com>
2024-09-03 17:57:44 -0400

    table commit

---
+++ tableview/DemoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DemoTableViewCell.swift
+//  tableview
+//
+//  Created by Vijay Kumar Munukoti on 9/3/24.
+//
+
+import UIKit
+
+class DemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ tableview/TableController.swift
@@ -0,0 +1,8 @@
+//
+//  TableController.swift
+//  tableview
+//
+//  Created by Vijay Kumar Munukoti on 9/3/24.
+//
+
+import Foundation
