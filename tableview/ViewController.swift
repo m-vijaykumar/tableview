7cb6c648047365374c12b6092cc79b8ac7613c72
Vijay Kumar <munukotivjiaykumar@gmail.com>
2024-09-03 16:34:51 -0400

    Initial Commit

---
+++ tableview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tableview
+//
+//  Created by Vijay Kumar Munukoti on 9/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
